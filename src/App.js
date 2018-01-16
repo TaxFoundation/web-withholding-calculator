52f120c70808b864821c0c9ab5dfaaacd325ec65
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-01-16 15:35:53 -0500

    Scaffold app functions to update state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
+import FilingStatusSelector from './components/FilingStatusSelector';
+import IncomeInput from './components/IncomeInput';
+import PayrollPeriodSelector from './components/PayrollPeriodSelector';
+import AllowancesInput from './components/AllowancesInput';
+import ResultsTable from './components/ResultsTable';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+  constructor() {
+    super();
 
-      </div>
-    );
+    this.state = {
+      income: 0,
+      filingStatus: 'single',
+      payrollPeriod: 'semimonthly',
+      allowances: 2
+    };
+
+    this.updateIncome = this.updateIncome.bind(this);
+    this.updateFilingStatus = this.updateFilingStatus.bind(this);
+    this.updatePayrollPeriod = this.updatePayrollPeriod.bind(this);
+  }
+
+  updateIncome(income) {
+    if (typeof income === 'number' && income >= 0) {
+      this.setState({ income });
+    }
+  }
+
+  updateFilingStatus(filingStatus) {
+    const statuses = ['single', 'married'];
+    if (statuses.indexOf(filingStatus) >= 0) {
+      this.setState({ filingStatus });
+    }
+  }
+
+  updatePayrollPeriod(payrollPeriod) {
+    const periods = [
+      'daily',
+      'weekly',
+      'biweekly',
+      'semimonthly',
+      'monthly',
+      'quarterly',
+      'semiannually',
+      'annually',
+    ];
+    if (periods.indexOf(payrollPeriod) >= 0) {
+      this.setState({payrollPeriod});
+    }
+  }
+
+  updateAllowances(allowances) {
+    if (typeof allowances === 'number') {
+      this.setState({allowances});
+    }
+  }
+
+  render() {
+    return <div className="App" />;
   }
 }
 
