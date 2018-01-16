5b3e7e8b1cb6850f96463ed1166a0d8564f091d3
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-01-16 15:48:44 -0500

    Create bulk of income input component

--- src/components/IncomeInput.js
+++ src/components/IncomeInput.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const IncomeInput = props => {};
+const Input = styled.input`
+  border: 1px solid #333333;
+  border-radius: 4px;
+  -moz-appearance: textfield;
+
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+  }
+`;
+
+const IncomeInput = props => {
+  return <Input type="number" onChange={e => props.update(e.target.value)} />;
+};
 
 export default IncomeInput;
