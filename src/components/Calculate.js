5bb6fd803d6870575a50b7983cb3f569e3538259
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-01-17 13:05:40 -0500

    Refactor Calculate to move payroll period date to config

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -23,44 +23,19 @@ const calcTax = (brackets, income) => {
 const getWithholdingTax = (payrollPeriod, annualWage, filingStatus, taxpayerAllowances) => {
   const preTCJA = config.policies.filter(p => p.id === 'pre-tcja')[0];
   const TCJA = config.policies.filter(p => p.id === 'tcja')[0];
-  const yearlyPayrollPeriods = [
-    { type: 'daily', value: 260 },
-    { type: 'weekly', value: 52 },
-    { type: 'biweekly', value: 26 },
-    { type: 'semimonthly', value: 24 },
-    { type: 'monthly', value: 12 },
-    { type: 'quarterly', value: 4 },
-    { type: 'semiannually', value: 2 },
-    { type: 'annually', value: 1 }
-  ];
-  const roundingPayrollPeriods = [
-    { type: 'daily', value: 1 },
-    { type: 'weekly', value: 0 },
-    { type: 'biweekly', value: 0 },
-    { type: 'semimonthly', value: 0 },
-    { type: 'monthly', value: 0 },
-    { type: 'quarterly', value: 0 },
-    { type: 'semiannually', value: 0 },
-    { type: 'annually', value: 0 }
-  ];
 
   const personal_exemption = preTCJA['personal_exemption'];
   const standard_deduction = preTCJA['standard_deduction'][filingStatus];
   const tcja_standard_deduction = TCJA['standard_deduction'][filingStatus];
-  const payrollPeriodValue = yearlyPayrollPeriods.filter(p => p.type === payrollPeriod)[0]['value'];
-  const roundingPayrollPeriodValue = roundingPayrollPeriods.filter(p => p.type === payrollPeriod)[0]['value'];
+  const payrollPeriodValue = config.yearlyPayrollPeriods.filter(p => p.type === payrollPeriod)[0];
 
-  const payroll_wage = annualWage / payrollPeriodValue;
-
-  const allowances = yearlyPayrollPeriods.map(p => {
-    return {type: p.type, allowance: precisionRound(personal_exemption / p.value, 1)};
-  });
+  const payroll_wage = annualWage / payrollPeriodValue.value;
 
   const preTCJABrackets = preTCJA.brackets[filingStatus].map(b => {
     let newThreshold = precisionRound(
       (b.threshold + standard_deduction - personal_exemption)
-      / payrollPeriodValue,
-      roundingPayrollPeriodValue
+      / payrollPeriodValue.value,
+      payrollPeriodValue.round
     );
     return {rate: b.rate, threshold: newThreshold};
   });
@@ -70,13 +45,13 @@ const getWithholdingTax = (payrollPeriod, annualWage, filingStatus, taxpayerAllo
       (b.threshold + tcja_standard_deduction - (
         filingStatus === 'single' ? 2 * personal_exemption : 3 * personal_exemption
       ))
-      / payrollPeriodValue,
-      roundingPayrollPeriodValue
+      / payrollPeriodValue.value,
+      payrollPeriodValue.round
     );
     return {rate: b.rate, threshold: newThreshold};
   });
 
-  const step1 = taxpayerAllowances * allowances.filter(a => a.type === payrollPeriod)[0]['allowance'];
+  const step1 = taxpayerAllowances * precisionRound(personal_exemption / payrollPeriodValue.value, 1);
   const step2 = payroll_wage - step1;
   const preTCJAWithheldTax = calcTax(preTCJABrackets, step2);
   const TCJAWithheldTax = calcTax(TCJABrackets, step2);
