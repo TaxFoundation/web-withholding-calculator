a3021f9d35acb0c83a1e26f4539c3450ea2c0637
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-01-17 15:39:37 -0500

    Add filing status selector

--- src/components/FilingStatusSelector.js
+++ src/components/FilingStatusSelector.js
@@ -2,7 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FilingStatusSelector = props => {
-  return <div></div>;
+  const statuses = ['single', 'married'];
+  return (
+    <form onChange={e => props.update(e.target.value)}>
+      {statuses.map(s => {
+        return (
+          <div key={`status-${s}`}>
+            <label htmlFor={s}>{s.charAt(0).toUpperCase() + s.slice(1)}</label>
+            <input type="radio" name="filing-status" id={s} value={s} defaultChecked={s === props.initialValue} />
+          </div>
+        );
+      })}
+    </form>
+  );
 };
 
 export default FilingStatusSelector;
